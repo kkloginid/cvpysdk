c29a66cef91aee18563dec77afbc0cb191baf2b1
Kuldeep Kumar <kkloginid@gmail.com>
2017-08-18 12:22:21 -0400

    import is missing for bencode method.
    committing in forked branch.

--- cvpysdk/storage.py
+++ cvpysdk/storage.py
@@ -110,7 +110,7 @@ SchedulePolicies:
 
 from __future__ import absolute_import
 from future.standard_library import install_aliases
-
+from base64 import b64encode
 from .exception import SDKException
 
 install_aliases()
